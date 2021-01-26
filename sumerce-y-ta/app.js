be8ab878e72b80f83c9fc209c69b2b28321a17c8
EHortua <e.hortua@gmail.com>
2021-01-26 17:49:53 -0500

    get registro y login

--- sumerce-y-ta/app.js
+++ sumerce-y-ta/app.js
@@ -8,6 +8,14 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./views/index.html"));
 });
 
+app.get("/registro", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/register.html"));
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/login.html"));
+});
+
 app.use("/static", express.static(__dirname + "/public"));
 
 app.listen(port, () => {
