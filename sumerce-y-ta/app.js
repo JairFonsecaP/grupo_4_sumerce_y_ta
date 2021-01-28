6874b107949ce802ee405530baf5c66bb1c7f52b
joon1ta <armestojona@gmail.com>
2021-01-28 02:38:30 -0300

    Agregado un padding nuevo al footer para separar, html y css de carrito 90% completado.

--- sumerce-y-ta/app.js
+++ sumerce-y-ta/app.js
@@ -12,6 +12,10 @@ app.get("/producto", (req, res) => {
   res.sendFile(path.join(__dirname, "./views/product.html"));
 });
 
+app.get("/cart", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/cart.html"));
+});
+
 app.use("/static", express.static(__dirname + "/public"));
 
 app.listen(port, () => {
