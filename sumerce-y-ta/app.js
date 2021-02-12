4755371f5fb4366b9646e1b7765510f2848ca35c
JairFonsecaP <jairfonseca64@yahoo.esa>
2021-02-11 22:33:34 -0300

    se crea carpeta controllers y una de routers, se cambia todas las vistar a ejs

--- sumerce-y-ta/app.js
+++ sumerce-y-ta/app.js
@@ -4,6 +4,9 @@ const path = require("path");
 
 const port = 3000;
 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./views/index.html"));
 });
