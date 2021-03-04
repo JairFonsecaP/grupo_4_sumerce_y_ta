a384ce8a529e900f48d2cce0545fce5849034e2a
JairFonsecaP <jairfonseca64@yahoo.esa>
2021-03-04 19:04:14 -0300

    Cambios en el creaated y el edit

--- sumerce-y-ta/app.js
+++ sumerce-y-ta/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const methodOverride = require("method-override");
 const router = require("./routes/index");
 
 const port = 3000;
@@ -8,6 +9,7 @@ const port = 3000;
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.use(methodOverride("_method"));
 app.use("/", router);
 
 app.use("/static", express.static(__dirname + "/public"));
--- sumerce-y-ta/controllers/productsController.js
+++ sumerce-y-ta/controllers/productsController.js
@@ -5,6 +5,17 @@ const uniqid = require("uniqid");
 const productsFilePath = path.join(__dirname, "../data/products.json");
 const products = JSON.parse(fs.readFileSync(productsFilePath, "utf-8"));
 const toThousand = (n) => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+const tallas = ["XS", "S", "M", "XL", "XLL"];
+const tonalidades = [
+  "Claro",
+  "Medio",
+  "Oscuro",
+  "Pasteles",
+  "Militar",
+  "Multicolor",
+  "Metalizados",
+];
+const tipos = ["Manga-larga", "Deportivas", "Top", "Manga-corta", "Sin-manga"];
 
 exports.carrito = (req, res) => {
   res.render("products/cart");
@@ -23,29 +34,45 @@ exports.producto = (req, res) => {
   let idDetail = req.params.id;
   products.forEach((product) => {
     if (product.id === idDetail) {
-      console.log(product);
       res.render("products/product", {
         product: product,
         toThousand: toThousand,
+        tallas: tallas,
       });
     }
   });
-
-  //res.render("products/product", { product: product, toThousand: toThousand });
 };
 
 exports.admproducto = (req, res) => {
-  res.render("products/admproduct");
+  res.render("products/admproduct", {
+    products: products,
+    toThousand: toThousand,
+  });
+};
+
+exports.create = (req, res) => {
+  res.render("products/createProduct", {
+    tallas: tallas,
+    tonalidades: tonalidades,
+    tipos: tipos,
+  });
 };
 
 exports.store = (req, res) => {
   let product = {};
-  let sizes = req.body.sizes;
-  let type = req.body.type;
-  let tonalidades = req.body.tonalidades;
-  sizes = sizes.toString();
-  type = type.toString();
-  tonalidades = tonalidades.toString();
+  let sizes = "";
+  let type = "";
+  let tonalidades = "";
+
+  if (typeof req.body.sizes === "string") {
+    sizes = [req.body.sizes];
+  }
+  if (typeof req.body.type === "string") {
+    types = [req.body.type];
+  }
+  if (typeof req.body.tonalidades === "string") {
+    tonalidades = [req.body.color];
+  }
 
   product.id = uniqid();
   product.name = req.body.name;
@@ -62,4 +89,53 @@ exports.store = (req, res) => {
   res.redirect("/products/admproducto");
 };
 
-exports.edit = (req, res) => {};
+exports.edit = (req, res) => {
+  const id = req.params.id;
+  products.forEach((product) => {
+    if (product.id === id) {
+      res.render("products/editProduct", {
+        product: product,
+        tallas: tallas,
+        tipos: tipos,
+        tonalidades: tonalidades,
+        toThousand: toThousand,
+      });
+    }
+  });
+};
+
+exports.update = (req, res) => {
+  let id = req.params.id;
+  let editProduct = req.body;
+  console.log(req.body);
+  let sizes = editProduct.sizes;
+  let type = editProduct.type;
+  let tonalidades = editProduct.color;
+
+  if (typeof req.body.sizes === "string") {
+    sizes = [req.body.sizes];
+  }
+  if (typeof req.body.type === "string") {
+    types = [req.body.type];
+  }
+  if (typeof req.body.tonalidades === "string") {
+    tonalidades = [req.body.tonalidades];
+  }
+
+  products.forEach((product) => {
+    if (product.id === id) {
+      product.name = editProduct.name;
+      product.categories = editProduct.categories;
+      product.color = tonalidades;
+      product.sizes = sizes;
+      product.description = editProduct.description;
+      product.price = editProduct.price;
+      product.type = type;
+
+      product.photo = editProduct.photo;
+    }
+  });
+  let edited = JSON.stringify(products);
+  fs.writeFileSync(path.join(__dirname, "../data/products.json"), edited);
+  res.redirect("/products/admproducto");
+};
